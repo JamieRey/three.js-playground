74fdebc4112f519d0c00cd6ee211268763494fb5
Jamie <jamie.rpr@gmail.com>
2022-06-25 20:50:06 +0100

    Big refactor of main, need to move away from monolith

--- vite-project/main.js
+++ vite-project/main.js
@@ -3,43 +3,97 @@ import './style.css'
 import * as THREE from 'three';
 import { MeshBasicMaterial } from 'three';
 
+function mainScene() {
+
+  rotatingDontScene();
+}
+
+
+
+// Region: Scenes
+//
+const donut = CreateDonut();
+function rotatingDontScene() {
+  const rotation = { x: 0.01, y: 0.005, z: 0.01,}
+  rotateObject(
+    rotation.x,
+    rotation.y,
+    rotation.z,
+    donut
+    )
+  }
+// End Region
+
+//
+  
+// Region: Objects
+//
+function CreateDonut() {
+  const shapeGeometry = { radius: 10, tube: 3, radialSegments: 16, tubularSegments: 100, }
+  const donutShape = new THREE.TorusBufferGeometry(
+    shapeGeometry.radius,
+    shapeGeometry.tube,
+    shapeGeometry.radialSegments,
+    shapeGeometry.tubularSegments
+  );
+  const material = new THREE.MeshBasicMaterial({
+    color : 0xFF6347,
+    wireframe: true,
+  });
+  return new THREE.Mesh(donutShape, material);
+}
+// End Region
+
+
+
+// Region: Object Manipulation
+//
+function rotateObject(x, y, z, object) {
+  object.rotation.x += x;
+  object.rotation.y += y;
+  object.rotation.z += z;
+}
+// End Region
+
+
+
+// Region: Utility
+function makeFullScreen() {
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+// End Region
+
+
+
+// Region: Animation Engine
+//
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
+const viewFrustrum = { fov: 75, aspect: window.innerWidth / window.innerHeight, near: 0.1, far: 1000, }
+const camera = new THREE.PerspectiveCamera( 
+  viewFrustrum.fov,
+  viewFrustrum.aspect,
+  viewFrustrum.near,
+  viewFrustrum.far,
 );
-const renderer = new THREE.WebGLRenderer({
-  canvas : document.querySelector('#bg'),
-});
+const webPageCanvas = document.querySelector('#bg')
+const renderer = new THREE.WebGLRenderer({ canvas : webPageCanvas, });
 
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
-
-renderer.render(scene, camera);
-
-// Creating a shape
-const geometry = new THREE.TorusBufferGeometry(10, 3, 16, 100);
-const material = new THREE.MeshBasicMaterial({
-  color : 0xFF6347,
-  wireframe: true,
-});
-const torus = new THREE.Mesh(geometry, material);
-
-scene.add(torus);
-
-// Game loop
-function animate() {
-  requestAnimationFrame( animate );
-
-  // Rotating object
-  torus.rotation.x += 0.01;
-  torus.rotation.y += 0.005;
-  torus.rotation.z += 0.01;
-
+function runEnviroment() {
+  requestAnimationFrame(runEnviroment);
+  mainScene();  
   renderer.render(scene, camera);
 }
 
-animate();
+function renderEnviroment() {
+  makeFullScreen();
+  camera.position.setZ(30);
+  scene.add(donut);
+  runEnviroment();
+}
+// End Region
+
+//
+
+// Main Script
+renderEnviroment()
